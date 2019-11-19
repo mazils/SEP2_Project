4d0b67904c6f447f6d708060818f54bf44d2ebc5
chukwukodinaka <285152@via.dk>
2019-11-19 14:02:37 +0100

    push hard

--- src/client/model/modelaccount/Accounts.java
+++ src/client/model/modelaccount/Accounts.java
@@ -1,4 +1,9 @@
 package client.model.modelaccount;
 
 public interface Accounts {
+    void setUsername(String Username);
+    void setPassword(String password);
+    String getPassword();
+    String getpassword();
+    boolean login();
 }

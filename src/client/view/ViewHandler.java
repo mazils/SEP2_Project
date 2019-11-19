02160866c27ffe9dcde0acbfc5896470e45fab08
Rafal Pierscieniak <279471@via.dk>
2019-11-19 16:46:14 +0100

    view handler

--- src/client/view/ViewHandler.java
+++ src/client/view/ViewHandler.java
@@ -1,4 +1,32 @@
 package client.view;
 
+import client.view.login.LoginController;
+import client.viewmodel.ViewModelFactory;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ViewHandler {
+    private Stage stage;
+    private ViewModelFactory vm;
+
+    public void openView(String viewToOpen){
+        Scene scene = null;
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = null;
+        try{
+            if(viewToOpen.equals("logIn")){
+                loader.setLocation(getClass().getResource("login/logIn.fxml"));
+                root = loader.load();
+                loader.<LoginController>getController().init(vm.getLoginViewModel(), this);
+            }
+
+
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+    }
 }

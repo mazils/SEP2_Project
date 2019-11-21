2153cfc0174f1e97695f8e2d4b6e2920c7f4095d
Rafal Pierscieniak <279471@via.dk>
2019-11-21 14:03:09 +0100

    view for create account

--- src/client/view/createaccount/CreateAccountController.java
+++ src/client/view/createaccount/CreateAccountController.java
@@ -1,4 +1,42 @@
 package client.view.createaccount;
 
+import client.view.ViewHandler;
+import client.viewmodel.createAccount.CreateAccountViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.control.CheckBox;
+
+
 public class CreateAccountController {
+    @FXML
+    TextField userNameTextField;
+
+    @FXML
+    TextField passwordTextField;
+
+    @FXML
+    TextField passwordConfTextField;
+
+    @FXML
+    CheckBox isManager;
+
+    private ViewHandler handler;
+    private CreateAccountViewModel viewModel;
+
+    public void init(ViewHandler viewHandler, CreateAccountViewModel createAccountViewModel){
+        handler=viewHandler;
+        viewModel=createAccountViewModel;
+
+        viewModel.getUserNameProperty().bindBidirectional(userNameTextField.textProperty());
+        viewModel.getPasswordProperty().bindBidirectional(passwordConfTextField.textProperty());
+        viewModel.getConfPasswordProperty().bindBidirectional(passwordConfTextField.textProperty());
+        viewModel.getIsManager().bindBidirectional(isManager.borderProperty());
+
+    }
+
+    public void onCreateButton(){
+        Stage stage = (Stage) userNameTextField.getScene().getWindow();
+        viewModel.createAccount(stage);
+    }
 }

59b32302c431e395557e0cff83c6fffcb5755cad
chukwukodinaka <285152@via.dk>
2019-11-28 15:59:59 +0100

    Add scooter

---
+++ src/client/viewmodel/sparePart/SparePartViewModel.java
@@ -0,0 +1,42 @@
+package client.viewmodel.sparePart;
+
+import client.model.SMModel.IMSModel;
+import client.model.SMModel.ISModel;
+import client.model.SMModel.ISparePart;
+import client.model.SMModel.SparePart;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class SparePartViewModel {
+    private IMSModel model;
+    private ListProperty<ISparePart> spareParts;
+    private ListProperty<ISModel> models;
+
+    public SparePartViewModel (IMSModel model)
+    {
+        this.model= model;
+        spareParts= new SimpleListProperty<>();
+        models= new SimpleListProperty<>();
+    }
+    public ListProperty<ISparePart> getSparePartsProperty() {
+        return spareParts;
+    }
+
+    public ListProperty<ISModel> getModelsProperty() {
+        return models;
+    }
+
+    public void getList(ISModel scooterModel)
+    {
+        Iterator<SparePart> sparePartIterator= model.getAllSpareparts(scooterModel).iterator();
+        while (sparePartIterator.hasNext())
+        {
+            spareParts.add(sparePartIterator.next());
+        }
+    }
+}

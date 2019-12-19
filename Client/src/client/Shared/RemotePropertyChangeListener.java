d5ba26cef7464dab7747d76c8eb7f1b08800f81e
Rafal Pierscieniak <279471@via.dk>
2019-12-19 14:54:05 +0100

    working version

--- Client/src/client/Shared/RemotePropertyChangeListener.java
+++
@@ -1,9 +0,0 @@
-package client.Shared;
-
-import java.beans.PropertyChangeEvent;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RemotePropertyChangeListener extends Remote {
-    void propertyChange(PropertyChangeEvent evt) throws RemoteException;
-}
